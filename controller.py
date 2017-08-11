5cc24bb9e2b4fcc64d058492276bbd53d932a0e7
McClane Howland <mcclane.howland@gmail.com>
2017-08-11 04:34:57 +0000

    probably the same

--- controller.py
+++ controller.py
@@ -23,9 +23,9 @@ c.close()
 h.close()
 followed_file = open("resources/followed.txt", "a")
 
-#######
+##########
 ##  URLs
-#######
+##########
 #TAG_URL = "https://www.instagram.com/explore/tags/%s/?__a=1&max_id=1"
 TAG_URL = "https://www.instagram.com/explore/tags/%s"
 PHOTO_BASE_URL = "https://www.instagram.com/p/%s"
@@ -35,7 +35,9 @@ PHOTO_BASE_URL = "https://www.instagram.com/p/%s"
 ## Start controlling the bot
 ##########################################
 #b = bot(input("username: "), getpass())
-b = bot("mcclane.howland", "$horecrest18")
+b = bot("joshuahowland","cali_boy")
+b.follow("taylorswift")
+b.unfollow("taylorswift")
 
 ##########################################
 ## Get initial following count
@@ -60,14 +62,14 @@ for i in range(howlong):
         b.like(photo_url=photo_urls[i])
         if(i == int(len(photo_urls)/2)):
             b.comment(message="You are so "+cl[randint(0, len(cl)-1)]+"!", photo_url=None)
-        #if(b.follow(usernames[i])):
-        #    followed_file.write(usernames[i]+"\n")
-        #    followed_list.append(usernames[i])
-        #    if(len(followed_list) < 50): 
-        #        time.sleep(delay)
-        #        continue
-        #    b.unfollow(followed_list[0]) 
-        #    followed_list.pop(0)
+        if(b.follow(usernames[i])):
+            followed_file.write(usernames[i]+"\n")
+            followed_list.append(usernames[i])
+            if(len(followed_list) < 50): 
+                time.sleep(delay)
+                continue
+            b.unfollow(followed_list[0]) 
+            followed_list.pop(0)
         time.sleep(delay)
 
 print("Non-unfollowed users: "+str(followed_list))
@@ -77,6 +79,7 @@ followed_file.close()
 # Unfollow all the leaks
 ###########################
 current_following_count = b.get_following_count()
+#current_following_count = 394
 leak = current_following_count - initial_following_count
 print(current_following_count, initial_following_count, leak)
 b.unfollow_n_most_recent(leak)
